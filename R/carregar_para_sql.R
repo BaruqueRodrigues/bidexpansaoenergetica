7f9e7aba706847e976a6b3b617bdeabd0df03456
Baruque Rodrigues <81967584+BaruqueRodrigues@users.noreply.github.com>
2024-04-22 17:17:11 -0300

    att funcao

--- R/carregar_para_sql.R
+++ R/carregar_para_sql.R
@@ -2,7 +2,7 @@
 #'
 #' Esta função verifica o sistema operacional, configura a localidade apropriada e utiliza duas funções auxiliares para conectar ao SQL Server e carregar dados de um dataframe especificado para uma tabela do banco de dados.
 #'
-#' @param dataframe Caminho para o arquivo CSV, separado por ";" que contém os dados a serem enviados para o banco de dados.
+#' @param dataframe dataset a ser carregado no ambiente do R para o SQL server
 #' @param nome_tabela Nome da tabela dentro do banco de dados onde os dados serão inseridos.
 #' @param servidor Endereço do servidor SQL Server.
 #' @param database Nome do banco de dados no servidor.
@@ -41,8 +41,6 @@ carregar_para_sql <- function(dataframe, nome_tabela, servidor, database, porta)
 
   con <- conectar_sql_server(servidor, database)
 
-  readr::read_csv2(dataframe)
-
   # Inserir os dados
   DBI::dbWriteTable(con, nome_tabela, dataframe, append = TRUE, row.names = FALSE)
 
@@ -50,14 +48,3 @@ carregar_para_sql <- function(dataframe, nome_tabela, servidor, database, porta)
   DBI::dbDisconnect(con)
 }
 
-#name_server <- "DESKTOP-66MU3PS\\SQLEXPRESS"
-#db_name <- "my_db"
-#user <- "sa"
-#key <- "eDTh291022;"
-
-#my_con <- conectar_sql_server(name_server, db_name)
-
-#df <- utils::read.csv(file="./data-aux/imdb.csv")
-
-#carregar_para_sql(dataframe="./data-aux/imgb.csv", nome_tabela="teste_table",
-#servidor=name_server, database="my_db")
