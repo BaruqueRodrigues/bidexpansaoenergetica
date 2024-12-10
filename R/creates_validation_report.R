95be7ad0e0854de87199c68723f25a9e23152c73
Icaro Bernardes <asaicaro@gmail.com>
2024-12-10 11:33:19 -0300

    validation report function

---
+++ R/creates_validation_report.R
@@ -0,0 +1,34 @@
+#' Create Validation Report
+#'
+#' Creates a HTML report to confirm the integrity
+#' of the wider and longer datasets.
+#'
+#' @param export_path A character string specifying the directory in
+#' which the longer and wider datasets are stored.
+#' Default is `"ETL_pipeline/data/data-output/"`.
+#'
+#' @return None. The function saves the report as a HTML file
+#' in the specified `export_path`.
+#'
+#' @details
+#' This function loads each specified `.rds` file containing the
+#' wider and longer datasets. It then invokes a .rmd script to
+#' create a HTML report that is saved on the `export_path`.
+#'
+#' @examples
+#' \dontrun{
+#' creates_validation_report(
+#'   export_path = "ETL_pipeline/data/data-output/"
+#' )
+#' }
+#'
+#' @export
+creates_validation_report <- function(
+    export_path = "ETL_pipeline/data/data-output/"
+){
+  rmarkdown::render(
+    input = system.file("validation_report.Rmd", package = "bidexpansaoenergetica"),
+    output_file = glue::glue("{export_path}validation_report.html"),
+    params = list(export_path = export_path)
+  )
+}
