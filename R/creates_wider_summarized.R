3c498abd169d3f0656de72b67cf89573079cbbd0
SamuelBRodrigues <sam.baraque@gmail.com>
2025-01-14 16:10:22 -0300

    Update summarized wider

--- R/creates_wider_summarized.R
+++ R/creates_wider_summarized.R
@@ -62,7 +62,7 @@ creates_wider_summarized <- function(dir = './ETL_pipeline/data/data-output/micr
     )
 
   # Aplicando tratamento para os NA
-  df_wider <- df_wider_comp |>
+  df_wider_amostra <- df_wider_comp |>
     mutate(
       across(starts_with("EA"),
              ~ replace(
@@ -72,120 +72,276 @@ creates_wider_summarized <- function(dir = './ETL_pipeline/data/data-output/micr
              )
       )
     )
+
+  df_wider_ponderado <- df_wider_amostra |>
+    dplyr::group_by(database, time) |>
+    dplyr::mutate(
+      dplyr::across(
+        dplyr::starts_with("EA"),
+        ~ .x * PESO_FINAL / sum(PESO_FINAL)
+      )
+    ) |>
+    dplyr::ungroup()
+
+  wider <- list(df_wider_amostra, df_wider_ponderado)
   #sumarização -----
 
-  df_wider_stats_BR <- df_wider |>
-    dplyr::mutate(geo = "country") |>
-    dplyr::mutate(geo_value = 0) |>
-    dplyr::group_by(database, time_period = "year", time, aggregation = 'household', geo, geo_value) |>
-    dplyr::summarise(across(starts_with("EA"),
-                            list(SUM = ~sum(.x, na.rm = T),
-                                 AVG = ~mean(.x, na.rm = T),
-                                 STD = ~sd(.x, na.rm = T),
-                                 CVR = ~ifelse(mean(.x, na.rm = T) == 0,
-                                               0,
-                                               sd(.x, na.rm = T) / mean(.x, na.rm = T)),
-                                 MED = ~quantile(.x, 0.5, , na.rm = T),
-                                 MIN = ~min(.x, na.rm = T),
-                                 Q01 = ~quantile(.x, 0.25, , na.rm = T),
-                                 Q03 = ~quantile(.x, 0.75, , na.rm = T),
-                                 MAX = ~max(.x, na.rm = T),
-                                 PDZ = ~sum(.x == 0, na.rm = T) / mean(data_size),
-                                 PDI = ~sum(.x == 9999, na.rm = T) / mean(data_size)),
-                            .names = "{col}_{.fn}")
-    ) |>
-    dplyr::ungroup() |>
-    tidyr::pivot_longer(
-      cols = -c('database','time_period', 'time', 'aggregation', 'geo', 'geo_value'),
-      names_to = c(".value", "stats_name"),
-      names_sep = "_"
-    ) |>
-    tidyr::pivot_longer(
-      cols = -c('database','time_period', 'time', 'aggregation', 'geo', 'geo_value', "stats_name"),
-    ) |>
-    dplyr::rename(stats_value_amostra = value,
-                  variable_id = name)
+  wider_stats_BR <- purrr::map2(
+    wider,
+    seq(1:2),
+    ~ {
+      df_wider = .x
+      iteration = .y
+      data <- df_wider |>
+        dplyr::mutate(geo = "country") |>
+        dplyr::mutate(geo_value = 0) |>
+        dplyr::group_by(database, time_period = "year", time, aggregation = 'household', geo, geo_value)
+      ifelse(iteration == 1,
+             # PARA A AMOSTRA
+             data <- data |>
+               dplyr::summarise(across(starts_with("EA"),
+                                       list(SUM = ~sum(.x, na.rm = T),
+                                            AVG = ~mean(.x, na.rm = T),
+                                            STD = ~sd(.x, na.rm = T),
+                                            CVR = ~ifelse(mean(.x, na.rm = T) == 0,
+                                                          0,
+                                                          sd(.x, na.rm = T) / mean(.x, na.rm = T)),
+                                            MED = ~quantile(.x, 0.5, , na.rm = T),
+                                            MIN = ~min(.x, na.rm = T),
+                                            Q01 = ~quantile(.x, 0.25, , na.rm = T),
+                                            Q03 = ~quantile(.x, 0.75, , na.rm = T),
+                                            MAX = ~max(.x, na.rm = T),
+                                            PDZ = ~sum(.x == 0, na.rm = T) / mean(data_size),
+                                            PDI = ~sum(.x == 9999, na.rm = T) / mean(data_size)),
+                                       .names = "{col}_{.fn}")
+               ) |>
+               dplyr::ungroup() |>
+               tidyr::pivot_longer(
+                 cols = -c('database','time_period', 'time', 'aggregation', 'geo', 'geo_value'),
+                 names_to = c(".value", "stats_name"),
+                 names_sep = "_"
+               ) |>
+               tidyr::pivot_longer(
+                 cols = -c('database','time_period', 'time', 'aggregation', 'geo', 'geo_value', "stats_name"),
+                 names_to = "variable_id",
+                 values_to = "stats_value_amostra"
+               ),
+             # APLICANDO O FATOR DE EXPANSÃO
+             data <- data |>
+               dplyr::summarise(across(starts_with("EA"),
+                                       list(SUM = ~sum(.x, na.rm = T),
+                                            AVG = ~mean(.x, na.rm = T),
+                                            STD = ~sd(.x, na.rm = T),
+                                            CVR = ~ifelse(mean(.x, na.rm = T) == 0,
+                                                          0,
+                                                          sd(.x, na.rm = T) / mean(.x, na.rm = T)),
+                                            MED = ~Hmisc::wtd.quantile(.x, weights = PESO_FINAL, probs = c(0.50)),
+                                            MIN = ~min(.x, na.rm = T),
+                                            Q01 = ~Hmisc::wtd.quantile(.x, weights = PESO_FINAL, probs = c(0.25)),
+                                            Q03 = ~Hmisc::wtd.quantile(.x, weights = PESO_FINAL, probs = c(0.75)),
+                                            MAX = ~max(.x, na.rm = T),
+                                            PDZ = ~sum(.x == 0, na.rm = T) / mean(data_size),
+                                            PDI = ~sum(.x == 9999, na.rm = T) / mean(data_size)),
+                                       .names = "{col}_{.fn}")
+               ) |>
+               dplyr::ungroup() |>
+               tidyr::pivot_longer(
+                 cols = -c('database','time_period', 'time', 'aggregation', 'geo', 'geo_value'),
+                 names_to = c(".value", "stats_name"),
+                 names_sep = "_"
+               ) |>
+               tidyr::pivot_longer(
+                 cols = -c('database','time_period', 'time', 'aggregation', 'geo', 'geo_value', "stats_name"),
+                 names_to = "variable_id",
+                 values_to = "stats_value_ponderado"
+               )
+      )
+      return(data)
+    }
+  )
+
+  df_wider_stats_BR <- dplyr::left_join(
+    wider_stats_BR[[1]],
+    wider_stats_BR[[2]]
+  )
 
 
 
-  df_wider_stats_UF <- df_wider |>
-    dplyr::mutate(geo = "uf") |>
-    dplyr::group_by(database, time_period = "year", time, aggregation = 'household', geo, geo_value) |>
-    dplyr::summarise(across(starts_with("EA"),
-                            list(SUM = ~sum(.x, na.rm = T),
-                                 AVG = ~mean(.x, na.rm = T),
-                                 STD = ~sd(.x, na.rm = T),
-                                 CVR = ~ifelse(mean(.x, na.rm = T) == 0,
-                                               0,
-                                               sd(.x, na.rm = T) / mean(.x, na.rm = T)),
-                                 MED = ~quantile(.x, 0.5, , na.rm = T),
-                                 MIN = ~min(.x, na.rm = T),
-                                 Q01 = ~quantile(.x, 0.25, , na.rm = T),
-                                 Q03 = ~quantile(.x, 0.75, , na.rm = T),
-                                 MAX = ~max(.x, na.rm = T),
-                                 PDZ = ~sum(.x == 0, na.rm = T) / mean(data_size),
-                                 PDI = ~sum(.x == 9999, na.rm = T) / mean(data_size)),
-                            .names = "{col}_{.fn}")
-    ) |>
-    dplyr::ungroup() |>
-    tidyr::pivot_longer(
-      cols = -c('database','time_period', 'time', 'aggregation', 'geo', 'geo_value'),
-      names_to = c(".value", "stats_name"),
-      names_sep = "_"
-    ) |>
-    tidyr::pivot_longer(
-      cols = -c('database','time_period', 'time', 'aggregation', 'geo', 'geo_value', "stats_name")) |>
-    dplyr::rename(stats_value_amostra = value,
-                  variable_id = name)
+  wider_stats_UF <- purrr::map2(
+    wider,
+    seq(1:2),
+    ~ {
+      df_wider = .x
+      iteration = .y
+      data <- df_wider |>
+        dplyr::mutate(geo = "uf") |>
+        dplyr::group_by(database, time_period = "year", time, aggregation = 'household', geo, geo_value)
+      ifelse(iteration == 1,
+             data <- data |>
+               dplyr::summarise(across(starts_with("EA"),
+                                       list(SUM = ~sum(.x, na.rm = T),
+                                            AVG = ~mean(.x, na.rm = T),
+                                            STD = ~sd(.x, na.rm = T),
+                                            CVR = ~ifelse(mean(.x, na.rm = T) == 0,
+                                                          0,
+                                                          sd(.x, na.rm = T) / mean(.x, na.rm = T)),
+                                            MED = ~quantile(.x, 0.5, , na.rm = T),
+                                            MIN = ~min(.x, na.rm = T),
+                                            Q01 = ~quantile(.x, 0.25, , na.rm = T),
+                                            Q03 = ~quantile(.x, 0.75, , na.rm = T),
+                                            MAX = ~max(.x, na.rm = T),
+                                            PDZ = ~sum(.x == 0, na.rm = T) / mean(data_size),
+                                            PDI = ~sum(.x == 9999, na.rm = T) / mean(data_size)),
+                                       .names = "{col}_{.fn}")
+               ) |>
+               dplyr::ungroup() |>
+               tidyr::pivot_longer(
+                 cols = -c('database','time_period', 'time', 'aggregation', 'geo', 'geo_value'),
+                 names_to = c(".value", "stats_name"),
+                 names_sep = "_"
+               ) |>
+               tidyr::pivot_longer(
+                 cols = -c('database','time_period', 'time', 'aggregation', 'geo', 'geo_value', "stats_name"),
+                 names_to = "variable_id",
+                 values_to = "stats_value_amostra"
+               ),
+             data <- data |>
+               dplyr::summarise(across(starts_with("EA"),
+                                       list(SUM = ~sum(.x, na.rm = T),
+                                            AVG = ~mean(.x, na.rm = T),
+                                            STD = ~sd(.x, na.rm = T),
+                                            CVR = ~ifelse(mean(.x, na.rm = T) == 0,
+                                                          0,
+                                                          sd(.x, na.rm = T) / mean(.x, na.rm = T)),
+                                            MED = ~Hmisc::wtd.quantile(.x, weights = PESO_FINAL, probs = c(0.50)),
+                                            MIN = ~min(.x, na.rm = T),
+                                            Q01 = ~Hmisc::wtd.quantile(.x, weights = PESO_FINAL, probs = c(0.25)),
+                                            Q03 = ~Hmisc::wtd.quantile(.x, weights = PESO_FINAL, probs = c(0.75)),
+                                            MAX = ~max(.x, na.rm = T),
+                                            PDZ = ~sum(.x == 0, na.rm = T) / mean(data_size),
+                                            PDI = ~sum(.x == 9999, na.rm = T) / mean(data_size)),
+                                       .names = "{col}_{.fn}")
+               ) |>
+               dplyr::ungroup() |>
+               tidyr::pivot_longer(
+                 cols = -c('database','time_period', 'time', 'aggregation', 'geo', 'geo_value'),
+                 names_to = c(".value", "stats_name"),
+                 names_sep = "_"
+               ) |>
+               tidyr::pivot_longer(
+                 cols = -c('database','time_period', 'time', 'aggregation', 'geo', 'geo_value', "stats_name"),
+                 names_to = "variable_id",
+                 values_to = "stats_value_ponderado"
+               )
+      )
+      return(data)
+    }
+  )
+
+  df_wider_stats_UF <- dplyr::left_join(
+    wider_stats_UF[[1]],
+    wider_stats_UF[[2]]
+  )
 
 
+  wider_stats_RG <- purrr::map2(
+    wider,
+    seq(1:2),
+    ~ {
+      df_wider = .x
+      iteration = .y
+      data <- df_wider |>
+        dplyr::mutate(geo = "region") |>
+        dplyr::mutate(geo_value = dplyr::case_when(
+          grepl("^1", as.character(geo_value)) == TRUE ~ 1, #Norte
+          grepl("^2", as.character(geo_value)) == TRUE ~ 2, #Nordeste
+          grepl("^3", as.character(geo_value)) == TRUE ~ 3, #Sudeste
+          grepl("^4", as.character(geo_value)) == TRUE ~ 4, #Sul
+          grepl("^5", as.character(geo_value)) == TRUE ~ 5, #Centro Oeste
+          TRUE ~ 100 #valor se erro
+        )) |>
+        dplyr::group_by(database, time_period = "year", time, aggregation = 'household', geo, geo_value)
+      ifelse(iteration == 1,
+             data <- data |>
+               dplyr::summarise(across(starts_with("EA"),
+                                       list(SUM = ~sum(.x, na.rm = T),
+                                            AVG = ~mean(.x, na.rm = T),
+                                            STD = ~sd(.x, na.rm = T),
+                                            CVR = ~ifelse(mean(.x, na.rm = T) == 0,
+                                                          0,
+                                                          sd(.x, na.rm = T) / mean(.x, na.rm = T)),
+                                            MED = ~quantile(.x, 0.5, , na.rm = T),
+                                            MIN = ~min(.x, na.rm = T),
+                                            Q01 = ~quantile(.x, 0.25, , na.rm = T),
+                                            Q03 = ~quantile(.x, 0.75, , na.rm = T),
+                                            MAX = ~max(.x, na.rm = T),
+                                            PDZ = ~sum(.x == 0, na.rm = T) / mean(data_size),
+                                            PDI = ~sum(.x == 9999, na.rm = T) / mean(data_size)),
+                                       .names = "{col}_{.fn}")
+               ) |>
+               dplyr::ungroup() |>
+               tidyr::pivot_longer(
+                 cols = -c('database','time_period', 'time', 'aggregation', 'geo', 'geo_value'),
+                 names_to = c(".value", "stats_name"),
+                 names_sep = "_"
+               ) |>
+               tidyr::pivot_longer(
+                 cols = -c('database','time_period', 'time', 'aggregation', 'geo', 'geo_value', "stats_name"),
+                 names_to = "variable_id",
+                 values_to = "stats_value_amostra"
+               ),
+             data <- data |>
+               dplyr::summarise(across(starts_with("EA"),
+                                       list(SUM = ~sum(.x, na.rm = T),
+                                            AVG = ~mean(.x, na.rm = T),
+                                            STD = ~sd(.x, na.rm = T),
+                                            CVR = ~ifelse(mean(.x, na.rm = T) == 0,
+                                                          0,
+                                                          sd(.x, na.rm = T) / mean(.x, na.rm = T)),
+                                            MED = ~Hmisc::wtd.quantile(.x, weights = PESO_FINAL, probs = c(0.50)),
+                                            MIN = ~min(.x, na.rm = T),
+                                            Q01 = ~Hmisc::wtd.quantile(.x, weights = PESO_FINAL, probs = c(0.25)),
+                                            Q03 = ~Hmisc::wtd.quantile(.x, weights = PESO_FINAL, probs = c(0.75)),
+                                            MAX = ~max(.x, na.rm = T),
+                                            PDZ = ~sum(.x == 0, na.rm = T) / mean(data_size),
+                                            PDI = ~sum(.x == 9999, na.rm = T) / mean(data_size)),
+                                       .names = "{col}_{.fn}")
+               ) |>
+               dplyr::ungroup() |>
+               tidyr::pivot_longer(
+                 cols = -c('database','time_period', 'time', 'aggregation', 'geo', 'geo_value'),
+                 names_to = c(".value", "stats_name"),
+                 names_sep = "_"
+               ) |>
+               tidyr::pivot_longer(
+                 cols = -c('database','time_period', 'time', 'aggregation', 'geo', 'geo_value', "stats_name"),
+                 names_to = "variable_id",
+                 values_to = "stats_value_ponderado"
+               )
+      )
+      return(data)
+    }
+  )
 
-  df_wider_stats_RG <- df_wider |>
-    dplyr::mutate(geo = "region") |>
-    dplyr::mutate(geo_value = dplyr::case_when(
-      grepl("^1", as.character(geo_value)) == TRUE ~ 1, #Norte
-      grepl("^2", as.character(geo_value)) == TRUE ~ 2, #Nordeste
-      grepl("^3", as.character(geo_value)) == TRUE ~ 3, #Sudeste
-      grepl("^4", as.character(geo_value)) == TRUE ~ 4, #Sul
-      grepl("^5", as.character(geo_value)) == TRUE ~ 5, #Centro Oeste
-      TRUE ~ 100 #valor se erro
-    )) |>
-    dplyr::group_by(database, time_period = "year", time, aggregation = 'household', geo, geo_value) |>
-    dplyr::summarise(across(starts_with("EA"),
-                            list(SUM = ~sum(.x, na.rm = T),
-                                 AVG = ~mean(.x, na.rm = T),
-                                 STD = ~sd(.x, na.rm = T),
-                                 CVR = ~ifelse(mean(.x, na.rm = T) == 0,
-                                               0,
-                                               sd(.x, na.rm = T) / mean(.x, na.rm = T)),
-                                 MED = ~quantile(.x, 0.5, , na.rm = T),
-                                 MIN = ~min(.x, na.rm = T),
-                                 Q01 = ~quantile(.x, 0.25, , na.rm = T),
-                                 Q03 = ~quantile(.x, 0.75, , na.rm = T),
-                                 MAX = ~max(.x, na.rm = T),
-                                 PDZ = ~sum(.x == 0, na.rm = T) / mean(data_size),
-                                 PDI = ~sum(.x == 9999, na.rm = T) / mean(data_size)),
-                            .names = "{col}_{.fn}")
-    ) |>
-    dplyr::ungroup() |>
-    tidyr::pivot_longer(
-      cols = -c('database','time_period', 'time', 'aggregation', 'geo', 'geo_value'),
-      names_to = c(".value", "stats_name"),
-      names_sep = "_"
-    ) |>
-    tidyr::pivot_longer(
-      cols = -c('database','time_period', 'time', 'aggregation', 'geo', 'geo_value', "stats_name")) |>
-    dplyr::rename(stats_value_amostra = value,
-                  variable_id = name)
-
-
+  df_wider_stats_RG <- dplyr::left_join(
+    wider_stats_RG[[1]],
+    wider_stats_RG[[2]]
+  )
 
   df_wider_stats <-
     df_wider_stats_BR |>
     dplyr::bind_rows(df_wider_stats_UF) |>
     dplyr::bind_rows(df_wider_stats_RG) |>
-    dplyr::mutate(stats_value_amostra = round(stats_value_amostra, 5)) |>
-    dplyr::relocate(stats_name, .before = stats_value_amostra)
+    dplyr::mutate(stats_value_amostra = round(stats_value_amostra, 5),
+                  stats_value_ponderado = round(stats_value_ponderado, 5)) |>
+    dplyr::relocate(stats_name, .before = stats_value_amostra) #%>%
+  dplyr::anti_join(
+    . |>
+      subset.data.frame(
+        database == "pnad" &
+          variable_id != c('EAIR0101', 'EAIR0102')
+      )
+  )
 
   df_wider_stats |> dplyr::glimpse()
 
@@ -194,3 +350,4 @@ creates_wider_summarized <- function(dir = './ETL_pipeline/data/data-output/micr
   df_wider_stats |>
     saveRDS(stringr::str_glue("{exdir}/_df_metrics_wider_summarized_by_EA.rds"))
 }
+
