a1dd790eda2a6998faecea4b4957fbceacaed60f
SamuelBRodrigues <sam.baraque@gmail.com>
2025-03-21 09:47:33 -0300

    Upgrade in cadunico function

--- R/baixa_cadunico_amostral.R
+++ R/baixa_cadunico_amostral.R
@@ -6,6 +6,7 @@
 #'
 #' @param destination_dir O caminho do diretório base onde os dados serão salvos. Por padrão,
 #' é 'data_raw/cadunico/'.
+#' @param ano Ano de referência dos dados a serem baixados.
 #'
 #' @return A função não retorna um valor explicitamente, mas como efeito colateral,
 #' arquivos são baixados e extraídos nos diretórios especificados.
@@ -17,7 +18,7 @@
 #'     destination_dir = 'data_raw/cadunico'
 #'   )
 #' }
-baixa_cadunico_amostral <- function(destination_dir = 'data_raw/cadunico/'){
+baixa_cadunico_amostral <- function(destination_dir = 'data_raw/cadunico/', ano = NULL){
 
   # Define a função para configurar a localidade baseada no sistema operacional
   set_locale_based_on_os <- function() {
@@ -40,152 +41,39 @@ baixa_cadunico_amostral <- function(destination_dir = 'data_raw/cadunico/'){
   # Mudando o tempo de espera máximo para o download para 10 minutos
   options(timeout = 600)
 
-  # Baixando os Dados de 2018 ----
-
-  # Checa a existência da pasta
-  if (!dir.exists(paste0(destination_dir,'2018'))) {
-    dir.create(paste0(destination_dir,'2018'), recursive = TRUE)
+  # Anos
+  if(is.null(ano)){
+    ano <- seq(2012,2018)
   }
 
-  # URL dos dados de 2018
-  url <- 'https://www.mds.gov.br/webarquivos/publicacao/sagi/microdados/01_cadastro_unico/base_amostra_cad_201812.zip'
+  # Baixando os Dados de  ----
 
-  # Destino do download
-  caminho_zip <- paste0(destination_dir,'2018/base_amostra_cad_201812.zip')
+  purrr::map(ano,
+             ~{
 
-  # Extraindo os dados de 2018
-  download.file(url,
-                destfile = caminho_zip,
-                mode = "wb")
-  # Extraindo os arquivos zip
-  archive::archive_extract(caminho_zip,
-                           dir = paste0(destination_dir,'2018/'))
+               # Checa a existência da pasta
+               if (!dir.exists(stringr::str_glue('{destination_dir}{.x}'))) {
+                 dir.create(stringr::str_glue('{destination_dir}{.x}'), recursive = TRUE)
+               }
 
+               # URL dos dados de 2018
+               url <- stringr::str_glue('https://www.mds.gov.br/webarquivos/publicacao/sagi/microdados/01_cadastro_unico/base_amostra_cad_{.x}12.zip')
 
-  # Baixando os dados de 2017 ------
+               # Destino do download
+               caminho_zip <- stringr::str_glue(
+                 '{destination_dir}{.x}/base_amostra_cad_{.x}12.zip'
+               )
 
-  # Checa a existência da pasta
-  if (!dir.exists(paste0(destination_dir,'2017'))) {
-    dir.create(paste0(destination_dir,'2017'), recursive = TRUE)
-  }
+               # Extraindo os dados de 2018
+               download.file(url,
+                             destfile = caminho_zip,
+                             mode = "wb")
+               # Extraindo os arquivos zip
+               archive::archive_extract(caminho_zip,
+                                        dir = stringr::str_glue('{destination_dir}{.x}/')
+               )
 
-  # URL dos dados de 2017
-  url <- 'https://www.mds.gov.br/webarquivos/publicacao/sagi/microdados/01_cadastro_unico/base_amostra_cad_201712.zip'
-
-  # Destino do download
-  caminho_zip <- paste0(destination_dir,'2017/base_amostra_cad_201712.zip')
-
-  # Extraindo os dados de 2017
-  download.file(url,
-                destfile = caminho_zip,
-                mode = "wb")
-  # Extraindo os arquivos ziop
-  archive::archive_extract(caminho_zip,
-                           dir = paste0(destination_dir,'2017/'))
-
-  # Baixando os dados de 2016 ------
-
-  # Checa a existência da pasta
-  if (!dir.exists(paste0(destination_dir,'2016'))) {
-    dir.create(paste0(destination_dir,'2016'), recursive = TRUE)
-  }
-
-  # URL dos dados de 2016
-  url <- 'https://www.mds.gov.br/webarquivos/publicacao/sagi/microdados/01_cadastro_unico/base_amostra_cad_201612.zip'
-
-  # Destino do download
-  caminho_zip <- paste0(destination_dir,'2016/base_amostra_cad_201612.zip')
-
-  # Extraindo os dados de 2016
-  download.file(url,
-                destfile = caminho_zip,
-                mode = "wb")
-  # Extraindo os arquivos ziop
-  archive::archive_extract(caminho_zip,
-                           dir = paste0(destination_dir,'2016/'))
-
-  # Baixando os dados de 2015 -------
-
-  # Checa a existência da pasta
-  if (!dir.exists(paste0(destination_dir,'2015'))) {
-    dir.create(paste0(destination_dir,'2015'), recursive = TRUE)
-  }
-
-  # URL dos dados de 2015
-  url <- 'https://www.mds.gov.br/webarquivos/publicacao/sagi/microdados/01_cadastro_unico/base_amostra_cad_201512.zip'
-
-  # Destino do download
-  caminho_zip <- paste0(destination_dir,'2015/base_amostra_cad_201512.zip')
-
-  # Extraindo os dados de 2015
-  download.file(url,
-                destfile = caminho_zip,
-                mode = "wb")
-  # Extraindo os arquivos ziop
-  archive::archive_extract(caminho_zip,
-                           dir = paste0(destination_dir,'2015/'))
-
-  # Baixando os dados de 2014 -------
-
-  # Checa a existência da pasta
-  if (!dir.exists(paste0(destination_dir,'2014'))) {
-    dir.create(paste0(destination_dir,'2014'), recursive = TRUE)
-  }
-
-  # URL dos dados de 2014
-  url <- 'https://www.mds.gov.br/webarquivos/publicacao/sagi/microdados/01_cadastro_unico/base_amostra_cad_201412.zip'
-
-  # Destino do download
-  caminho_zip <- paste0(destination_dir,'2014/base_amostra_cad_201412.zip')
-
-  # Extraindo os dados de 2014
-  download.file(url,
-                destfile = caminho_zip,
-                mode = "wb")
-  # Extraindo os arquivos ziop
-  archive::archive_extract(caminho_zip,
-                           dir = paste0(destination_dir,'2014/'))
-
-  # Baixando os dados de 2013 -------
-
-  # Checa a existência da pasta
-  if (!dir.exists(paste0(destination_dir,'2013'))) {
-    dir.create(paste0(destination_dir,'2013'), recursive = TRUE)
-  }
-
-  # URL dos dados de 2013
-  url <- 'https://www.mds.gov.br/webarquivos/publicacao/sagi/microdados/01_cadastro_unico/base_amostra_cad_201312.zip'
-
-  # Destino do download
-  caminho_zip <- paste0(destination_dir,'2013/base_amostra_cad_201312.zip')
-
-  # Extraindo os dados de 2013
-  download.file(url,
-                destfile = caminho_zip,
-                mode = "wb")
-  # Extraindo os arquivos ziop
-  archive::archive_extract(caminho_zip,
-                           dir = paste0(destination_dir,'2013/'))
-
-  # Baixando os dados de 2012 -------
-
-  # Checa a existência da pasta
-  if (!dir.exists(paste0(destination_dir,'2012'))) {
-    dir.create(paste0(destination_dir,'2012'), recursive = TRUE)
-  }
-
-  # URL dos dados de 2012
-  url <- 'https://www.mds.gov.br/webarquivos/publicacao/sagi/microdados/01_cadastro_unico/base_amostra_cad_201212.zip'
-
-  # Destino do download
-  caminho_zip <- paste0(destination_dir,'2012/base_amostra_cad_201212.zip')
-
-  # Extraindo os dados de 2012
-  download.file(url,
-                destfile = caminho_zip,
-                mode = "wb")
-  # Extraindo os arquivos ziop
-  archive::archive_extract(caminho_zip,
-                           dir = paste0(destination_dir,'2012/'))
+             }
+  )
 
 }
