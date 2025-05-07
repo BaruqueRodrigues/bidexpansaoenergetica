1da79b8a1dcea74cc57af180b9cbc9b3e7a22660
adelino-dev <adelino.developer@gmail.com>
2025-05-06 23:33:58 -0300

    fix: update anp_precglp_transform_wider and create_anp_precoglp_vulnerability to the discussed format

--- R/anp_precoglp_transform_wider.R
+++ R/anp_precoglp_transform_wider.R
@@ -13,160 +13,64 @@
 #' \dontrun{
 #'   anp_precoglp_transform_wider <- anp_precoglp_transform_wider()
 #' }
-anp_precoglp_transform_wider  <- function(dir= "data_raw/preco_glp/", file_name = "preco_glp_2004_2024.rds"){
+anp_precoglp_transform_wider <- function(dir = "data_raw/preco_glp/", file_name = "preco_glp_2004_2024.rds") {
+
+
   file_path <- system.file(dir, file_name, package = "bidexpansaoenergetica")
 
-  #Selecionado e organizando os dados brutos:
-  preco_glp <-  readRDS(file_path) |>
+  region_map <- tibble::tibble(
+    regiao_sigla = c("N", "NE", "CO", "SE", "S"),
+    region = as.character(1:5)
+  )
+
+  estado_map <- tibble::tibble(
+    estado_sigla = c(
+      "RO", "AC", "AM", "RR", "PA", "AP", "TO", "MA", "PI", "CE",
+      "RN", "PB", "PE", "AL", "SE", "BA", "MG", "ES", "RJ", "SP",
+      "PR", "SC", "RS", "MS", "MT", "GO", "DF"
+    ),
+    uf = c(
+      "11", "12", "13", "14", "15", "16", "17", "21", "22", "23",
+      "24", "25", "26", "27", "28", "29", "31", "32", "33", "35",
+      "41", "42", "43", "50", "51", "52", "53"
+    )
+  )
+
+  anp_precoglp_wider <- readRDS(file_path) |>
     janitor::clean_names() |>
     dplyr::mutate(
-      region = dplyr::case_match(regiao_sigla,
-                                 "N" ~ "1",
-                                 "NE" ~ "2",
-                                 "CO" ~ "3",
-                                 "SE" ~ "4",
-                                 "S" ~ "5"),
-      uf = dplyr::case_match(
-        `estado_sigla`,
-        "RO" ~ "11",
-        "AC" ~ "12",
-        "AM" ~ "13",
-        "RR" ~ "14",
-        "PA" ~ "15",
-        "AP" ~ "16",
-        "TO" ~ "17",
-        "MA" ~ "21",
-        "PI" ~ "22",
-        "CE" ~ "23",
-        "RN" ~ "24",
-        "PB" ~ "25",
-        "PE" ~ "26",
-        "AL" ~ "27",
-        "SE" ~ "28",
-        "BA" ~ "29",
-        "MG" ~ "31",
-        "ES" ~ "32",
-        "RJ" ~ "33",
-        "SP" ~ "35",
-        "PR" ~ "41",
-        "SC" ~ "42",
-        "RS" ~ "43",
-        "MS" ~ "50",
-        "MT" ~ "51",
-        "GO" ~ "52",
-        "DF" ~ "53"
-      ),
-      ano = data_da_coleta |> lubridate::dmy() |> lubridate::year()
+      data_da_coleta = lubridate::dmy(data_da_coleta),
+      ano = lubridate::year(data_da_coleta),
     )|>
-    dplyr::select(
-      region,
-      uf,
-      ano,
-      valor_de_venda,
-      cnpj_da_revenda
-    )
-
-  #Agregação por país:
-  preco_glp_br <- preco_glp |>
-    dplyr::mutate(
-      geo_value = '0'
-    ) |>
-    dplyr::select(
-      -uf,
-      -region
-    ) |>
-    dplyr::group_by(
-      geo_value,
-      ano,
-      cnpj_da_revenda
-    ) |>
+    dplyr::group_by(regiao_sigla, estado_sigla, cnpj_da_revenda, ano) |>
+    dplyr::arrange(data_da_coleta, .by_group = TRUE) |>
     dplyr::summarise(
-      dplyr::across(
-        everything(),
-        list(
-          AVG = ~mean(.x, na.rm = T)
-        ),
-        .names = "{.col}_{.fn}"
-      ),
+      valor_de_venda_AVG = mean(valor_de_venda),
+      valor_de_venda_MED = median(valor_de_venda),
+      valor_de_venda_SD = sd(valor_de_venda),
+      valor_de_venda_FIRST = first(valor_de_venda),
+      valor_de_venda_LAST = last(valor_de_venda),
+      valor_de_venda_COUNT = dplyr::n(),
       .groups = "drop"
     ) |>
-    dplyr::mutate(
-      geo = "country"
-    )
-
-  #Agregação por região:
-  preco_glp_reg <- preco_glp |>
-    dplyr::rename(
-      geo_value = region
-    ) |>
-    dplyr::select(
-      -uf
-    ) |>
-    dplyr::group_by(
-      geo_value,
-      ano,
-      cnpj_da_revenda
-    ) |>
-    dplyr::summarise(
-      dplyr::across(
-        everything(),
-        list(
-          AVG = ~mean(.x, na.rm = T)
-        ),
-        .names = "{.col}_{.fn}"
-      ),
-      .groups = "drop"
-    ) |>
-    dplyr::mutate(
-      geo = "region"
-    )
-
-  #Agregação por uf:
-  preco_glp_uf <- preco_glp |>
-    dplyr::rename(
-      geo_value = uf
-    ) |>
-    dplyr::select(
-      -region
-    ) |>
-    dplyr::group_by(
-      geo_value,
-      ano,
-      cnpj_da_revenda
-    ) |>
-    dplyr::summarise(
-      dplyr::across(
-        everything(),
-        list(
-          AVG = ~mean(.x, na.rm = T)
-        ),
-        .names = "{.col}_{.fn}"
-      ),
-      .groups = "drop"
-    ) |>
-    dplyr::mutate(
-      geo = "uf"
-    )
-
-
-  #Tabela wider final:
-  anp_precoglp_wider <- preco_glp_br |>
-    dplyr::bind_rows(preco_glp_reg) |>
-    dplyr::bind_rows(preco_glp_uf)|>
 
     dplyr::mutate(
       database = "anp_precoglp",
-      time_period = "year") |>
-
-    dplyr::rename(
-      time = ano
+      time_period = "year"
     ) |>
-
-    dplyr::relocate(
-      database, time_period, time, geo, geo_value,
-      cnpj_da_revenda, valor_de_venda_AVG
-    )
+    dplyr::rename(time = ano) |>
+    dplyr::left_join(region_map, by = "regiao_sigla") |>
+    dplyr::left_join(estado_map, by = "estado_sigla") |>
+    dplyr::select(database,
+                  region, uf,
+                  time_period, time,
+                  cnpj_da_revenda,
+                  valor_de_venda_AVG,
+                  valor_de_venda_MED,
+                  valor_de_venda_FIRST,
+                  valor_de_venda_LAST,
+                  valor_de_venda_COUNT,
+                  valor_de_venda_SD)
 
   return (anp_precoglp_wider)
-
 }
